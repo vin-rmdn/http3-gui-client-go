3fbb54350c899f3bde5b410b39a9225ed98ddf6b
Alvin Januar <aljan_r@outlook.co.id>
2025-09-02 00:26:32 +0700

    feat: use both logger and config in main
    Signed-off-by: Alvin Januar <aljan_r@outlook.co.id>

--- main.go
+++ main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"log/slog"
 	"os"
+
+	"github.com/vin-rmdn/http3-gui-client-go/internal/config"
+	"github.com/vin-rmdn/http3-gui-client-go/internal/logger"
 )
 
 func main() {
-	log := slog.New(slog.NewTextHandler(
-		os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-	)
-	slog.SetDefault(log)
+	conf := config.Load("./config.yml")
 
-	slog.Info("Hello, world!")
+	log, err := logger.Setup(conf)
+	if err != nil {
+		slog.Error(
+			"cannot unmarshal log level",
+			slog.String("level", conf.Log.Level),
+		)
+
+		const exitCodeDataError = 65
+		os.Exit(exitCodeDataError)
+	}
+	log.Info("Hello, world!", slog.String("todays_message", conf.Today.Message))
 }
